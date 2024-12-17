83a354b0d82f1418285c4be2773004d497fa5509
Toni <dayhlprod@gmail.com>
2024-12-17 21:26:27 +0000

    Added mild styling to login form

--- src/pages/components/Login.jsx
+++ src/pages/components/Login.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Collapse } from 'react-bootstrap';
 
 const Login = ({ setUser }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isError, setError] = useState(false);
   const [isLoading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const storedUsername = 'User1';
   const storedPassword = 'password12345@';
@@ -41,45 +42,69 @@ const Login = ({ setUser }) => {
 
   if (token && usernameInStorage) {
     return (
-      <div className="login">
-        <h4>Welcome, {usernameInStorage}</h4>
-        <Button variant="outline-dark" onClick={handleLogout}>
-          Logout
+      <>
+        <Button
+          onClick={() => setOpen(!open)}
+          aria-controls="open-login-form"
+          aria-expanded={open}
+          variant="outline-dark"
+        >
+          {open ? 'Hide Logout Option' : 'Show Logout Option'}
         </Button>
-      </div>
+        <Collapse in={open}>
+          <div className="login">
+            <h4>Welcome, {usernameInStorage}</h4>
+            <Button variant="outline-dark" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        </Collapse>
+      </>
     );
   }
 
   return (
-    <div className="login">
-      <h2>Login</h2>
-      <Form onSubmit={handleLogin}>
-        <section id="username-form">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          ></input>
-        </section>
-        <section id="password-form">
-          <label htmlFor="password">Password</label>
-          <input
-            type="text"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          ></input>
-        </section>
-        {isError && <p>{error}</p>}
-        <Button type="submit" variant="outline-dark" disabled={isLoading}>
-          {isLoading ? 'Logging in...' : 'Login'}
-        </Button>
-      </Form>
-    </div>
+    <>
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="open-login-form"
+        aria-expanded={open}
+        variant="outline-dark"
+      >
+        {open ? 'Hide Login Menu' : 'Show Login Menu'}
+      </Button>
+      <Collapse in={open}>
+        <div className="login">
+          <h2>Login</h2>
+          <Form onSubmit={handleLogin}>
+            <section id="username-form">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              ></input>
+            </section>
+            <section id="password-form">
+              <label htmlFor="password">Password</label>
+              <input
+                type="text"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              ></input>
+            </section>
+            {isError && <p>{error}</p>}
+            <Button type="submit" variant="outline-dark" disabled={isLoading}>
+              {isLoading ? 'Logging in...' : 'Login'}
+            </Button>
+          </Form>
+        </div>
+      </Collapse>
+    </>
   );
 };
 export default Login;
